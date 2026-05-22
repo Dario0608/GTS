1ef95e455e54622d08695193805b649e053cdfca
Dario0608 <rochemarind@gmail.com>
2026-05-22 14:37:16 +0200

    Diseño arcade 80s, efectos de sonido web audio api, temblor de pantalla y mejoras en replay

--- GTS.js
+++ GTS.js
@@ -3,21 +3,30 @@ let reproductorAudio = null;
 let vidas = 3;
 let puntos = 0;
 let tiempoPermitido = 5;
+let timeoutSugerencias = null;
+
+const contenedorJuego = document.getElementById("gameContainer");
 
 const barraProgreso = document.getElementById("progressBar");
 const start = document.getElementById("play");
 const zonaJuego = document.getElementById("gameZone");
+
 const entrada = document.getElementById("guess");
 const comprobar = document.getElementById("check");
 const respuesta = document.getElementById("feedback");
 const intentos = document.getElementById("hearts");
+
+const botonReplay = document.getElementById("replayBtn");
 const siguienteCancion = document.getElementById("next");
 const marcadorPuntos = document.getElementById("marcador");
-const selectorGenero = document.getElementById("genreSelector");
+const entradaGenero = document.getElementById("genreSelector");
+const contadorTiempo = document.getElementById("timeCounter");
+const listaSugerencias = document.getElementById("suggestion");
+
 
 
 async function buscarCancion(genero) {
-    const url = `https://itunes.apple.com/search?term=${genero}&entity=song&limit=5`;
+    const url = `https://itunes.apple.com/search?term=${genero}&entity=song&limit=50`;
 
     try {
         const respuesta = await fetch(url);
@@ -36,9 +45,9 @@ async function buscarCancion(genero) {
         reproductorAudio = new Audio(cancionSecreta.previewUrl);
 
         reproductorAudio.addEventListener("timeupdate", () => {
+            barraProgreso.value = reproductorAudio.currentTime;
+            contadorTiempo.innerText = `${reproductorAudio.currentTime.toFixed(1)}s / 30s`;
 
-            const porcentaje = (reproductorAudio.currentTime / tiempoPermitido) * 100;
-            barraProgreso.value = porcentaje;
             if (reproductorAudio.currentTime >= tiempoPermitido) {
                 reproductorAudio.pause();
                 reproductorAudio.currentTime = 0;
@@ -65,11 +74,13 @@ function limpiarTexto(texto) {
 }
 
 start.addEventListener("click", () => {
+    const generoElegido = entradaGenero.value.trim();
+    if (!generoElegido) return;
+
     console.log("Iniciando juego...");
-    const generoElegido = selectorGenero.value;
     buscarCancion(generoElegido);
 
-    selectorGenero.style.display = "none";
+    entradaGenero.style.display = "none";
     start.style.display = "none";
     siguienteCancion.style.display = "none";
     zonaJuego.style.display = "block";
@@ -85,6 +96,7 @@ comprobar.addEventListener("click", () => {
         respuesta.style.color = "green";
 
         if (reproductorAudio) reproductorAudio.pause();
+        playSonidoAcierto();
 
         comprobar.style.display = "none";
         siguienteCancion.style.display = "block";
@@ -95,20 +107,32 @@ comprobar.addEventListener("click", () => {
         respuesta.innerText = "Incorrect";
         respuesta.style.color = "red";
 
+        if (reproductorAudio) reproductorAudio.pause();
+        playSonidoError();
+        contenedorJuego.classList.add("shake-error");
+
+        setTimeout(() => {
+            contenedorJuego.classList.remove("shake-error");
+
+        }, 400);
+
         --vidas;
         intentos.innerText = `Lifes left: ${vidas}`;
 
         if (vidas == 2) {
             tiempoPermitido = 15;
-            reproductorAudio.play();
+            setTimeout(() => {
+                if (reproductorAudio) reproductorAudio.play();
+            }, 800);
         } else if (vidas == 1) {
             tiempoPermitido = 30;
-            reproductorAudio.play();
+            setTimeout(() => {
+                if (reproductorAudio) reproductorAudio.play();
+            }, 800);
         } else if (vidas == 0) {
-            respuesta.innerText = "You Lost :/";
+            respuesta.innerText = `You Lost :/ The song was: ${cancionSecreta.trackName} by ${cancionSecreta.artistName}`;
             comprobar.style.display = "none";
             siguienteCancion.style.display = "block";
-            if (reproductorAudio) reproductorAudio.pause();
         }
     }
 
@@ -117,17 +141,104 @@ comprobar.addEventListener("click", () => {
 siguienteCancion.addEventListener("click", () => {
     respuesta.innerText = "";
     entrada.value = "";
-    siguienteCancion.style.display = "none";
-    comprobar.style.display = "block";
-
-    const generoActual = selectorGenero.value;
-    buscarCancion(generoActual);
-
+    listaSugerencias.innerHTML = "";
     barraProgreso.value = 0;
+    contadorTiempo.innerText = "0.0s / 30s";
     vidas = 3;
     tiempoPermitido = 5;
     intentos.innerText = `Lifes left: ${vidas}`;
+
+    zonaJuego.style.display = "none";
+    siguienteCancion.style.display = "none";
+    comprobar.style.display = "block";
+
+    entradaGenero.style.display = "inline-block";
+    entradaGenero.value = "";
+    start.style.display = "inline-block";
+
+
 });
 
+botonReplay.addEventListener("click", () => {
+    if (reproductorAudio) {
 
+        if (!reproductorAudio.paused()) {
+            reproductorAudio.pause();
+        } else {
+            reproductorAudio.currentTime = 0;
+            reproductorAudio.play();
+        }
 
+    }
+})
+
+entrada.addEventListener("input", () => {
+    clearTimeout(timeoutSugerencias);
+    const textoUsuario = entrada.value.trim();
+
+    if (textoUsuario.length < 3) {
+        listaSugerencias.innerHTML = "";
+        return;
+    }
+
+    timeoutSugerencias = setTimeout(async () => {
+        const url = `https://itunes.apple.com/search?term=${textoUsuario}&entity=song&limit=5`;
+
+        try {
+            const respuesta = await fetch(url);
+            const datos = await respuesta.json();
+
+            listaSugerencias.innerHTML = "";
+
+            datos.results.forEach(cancion => {
+                const opcion = document.createElement("option");
+                opcion.value = cancion.trackName;
+                listaSugerencias.appendChild(opcion);
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    }, 200);
+
+});
+
+function playSonidoError() {
+    const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+
+    const oscilador = audioCtx.createOscillator();
+    const volumen = audioCtx.createGain();
+
+    oscilador.type = "square";
+    oscilador.frequency.setValueAtTime(160, audioCtx.currentTime);
+
+    oscilador.frequency.exponentialRampToValueAtTime(70, audioCtx.currentTime + 0.3);
+
+    volumen.gain.setValueAtTime(0.3, audioCtx.currentTime);
+    volumen.gain.exponentialRampToValueAtTime(0.01, audioCtx.currentTime + 0.3);
+
+    oscilador.connect(volumen);
+    volumen.connect(audioCtx.destination);
+
+    oscilador.start();
+    oscilador.stop(audioCtx.currentTime + 0.3);
+}
+
+function playSonidoAcierto() {
+    const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+    const oscilador = audioCtx.createOscillator();
+    const volumen = audioCtx.createGain();
+
+    oscilador.type = "square";
+
+    oscilador.frequency.setValueAtTime(523.25, audioCtx.currentTime);
+    oscilador.frequency.setValueAtTime(880.00, audioCtx.currentTime + 0.08);
+
+    volumen.gain.setValueAtTime(0.2, audioCtx.currentTime);
+    volumen.gain.exponentialRampToValueAtTime(0.01, audioCtx.currentTime + 0.3);
+
+    oscilador.connect(volumen);
+    volumen.connect(audioCtx.destination);
+
+    oscilador.start();
+    oscilador.stop(audioCtx.currentTime + 0.3);
+}
